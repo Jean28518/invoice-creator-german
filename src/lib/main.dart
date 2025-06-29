987f37cbbe9bd09669e0f2d2b1eaa75f75c8d400
Jean28518 <jf_v@gmx.de>
2025-06-29 18:15:58 +0100

    Disable window manager plugin due to build errors

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:invoice/pages/loading_screen.dart';
 import 'package:invoice/widgets/mint_y.dart';
-import 'package:window_manager/window_manager.dart';
+// import 'package:window_manager/window_manager.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await WindowManager.instance.ensureInitialized();
-  WindowManager.instance.setTitle("Rechnungs Assistent");
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await WindowManager.instance.ensureInitialized();
+  // WindowManager.instance.setTitle("Rechnungs Assistent");
 
   runApp(const MyApp());
 }
